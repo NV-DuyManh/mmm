f16a05265396bd4d349e165852003d5a652d27db
Đặng Đại Việt <vdang9721@gmail.com>
2026-03-01 09:59:07 +0700

    Viet commit and update PagePhim

--- Cinema/Cinema/PagePhim.xaml.cs
+++ Cinema/Cinema/PagePhim.xaml.cs
@@ -135,5 +135,9 @@ namespace Cinema
                 }
             }
         }
+        private void dgPhim_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
     }
 }
